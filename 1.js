19084e665ecc9329580a31bea9cde1f8675568bb
subhash7995 <134258964+subhash7995@users.noreply.github.com>
2024-10-18 22:05:46 +0530

    2300080203_Boggavarapu Subhash

---
+++ 1.js
@@ -0,0 +1,20 @@
+function add(a,b){
+    return a+b;
+}
+function dif(a,b){
+    return a-b;
+}
+function mul(a,b){
+    return a*b;
+}
+function div(a,b){
+    return a/b;
+}
+
+let a = 20
+let b = 10
+console.log(add(a,b))
+console.log(dif(a,b))
+console.log(mul(a,b))
+console.log(div(a,b))
+
---
+++ sum.js
@@ -0,0 +1,26 @@
+let a = 40;
+let b = 30;
+const add = (a,b)=>{
+    return a+b;
+    
+}
+
+
+const diff = (a,b)=>{
+    return a-b;
+    
+}
+
+const mul= (a,b)=>{
+    return a*b;
+    
+}
+
+const div = (a,b)=>{
+    return a/b;
+
+}
+console.log(add(a,b));
+console.log(diff(a,b));
+console.log(mul(a,b));
+console.log(div(a,b));
